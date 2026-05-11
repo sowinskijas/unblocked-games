e84ec321a0683de45f51cc8fc8188c6fe3cbf25d
sowinskijas <sowinskijas@gmail.com>
2026-05-11 08:47:58 -0400

    auto: update register-sw.js

--- public/register-sw.js
+++ public/register-sw.js
@@ -1,10 +1,7 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/uv/uv.sw.js', { scope: '/uv/service/' })
-    .then(() => console.log('UV Service Worker registered'))
+    .then(reg => {
+      console.log('UV Service Worker registered', reg.scope);
+    })
     .catch(err => console.error('SW registration failed:', err));
-}
-
-function uvGo(url) {
-  if (!url.startsWith('http')) url = 'https://' + url;
-  location.href = '/uv/service/' + __uv$config.encodeUrl(url);
 }
